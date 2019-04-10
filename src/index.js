86e59a1b76eedd3f8725791c4566ba177dc25077
Yannick Lohse <contact@yannick-lohse.fr>
2019-04-10 16:51:43 +0200

    feat: Get the current account id

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { BaseKonnector, log } = require('cozy-konnector-libs')
+const { BaseKonnector, log, errors } = require('cozy-konnector-libs')
+const getAccountId = require('./helpers/getAccountId')
 
 module.exports = new BaseKonnector(start)
 
@@ -6,7 +7,7 @@ module.exports = new BaseKonnector(start)
 // information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
 // the account information come from ./konnector-dev-config.json file
 async function start() {
-  log('info', 'Nothing to run yet ...')
+  log('info', 'Starting the Toutatice connector')
 
   // get or create contacts.account
   //
@@ -20,4 +21,11 @@ async function start() {
   // - fetch cozy contact
   // - - if inexistent, create it
   // - - else update by overriding with remote first
+  try {
+    const accountId = getAccountId()
+  } catch (err) {
+    log('error', 'caught an unexpected error')
+    log('error', err.message)
+    throw errors.VENDOR_DOWN
+  }
 }
